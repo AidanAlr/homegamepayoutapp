65b693a7472fcbea2cd5ebe8c377e1bc93d219a0
aidan <aidanalrawi@icloud.com>
2024-08-13 13:25:06 -0400

    Commit message goes here

---
+++ README.md
@@ -0,0 +1,9 @@
+# Homegamepayoutapp
+
+{{ appDescription }}
+
+[Releases](https://github.com/aidanalr/homegamepayoutapp/releases)
+
+## About this Project
+
+This project was generated using the Swing project template from [jDeploy](https://www.jdeploy.com), a tool to deploy Java desktop apps on Windows, Mac.
---
+++ src/test/java/com/github/aidanalr/homegamepayoutapp/HomegamepayoutappTest.java
@@ -0,0 +1,38 @@
+package com.github.aidanalr.homegamepayoutapp;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class HomegamepayoutappTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public HomegamepayoutappTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( HomegamepayoutappTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
